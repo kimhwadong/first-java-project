e57c5c9638fdc151230c56b63d56755f7bb01f0a
hwadong kim <HWADONG@DESKTOP-9000T34>
2022-03-30 21:32:52 +0900

    This is Commit Message

---
+++ src/javawork/work1.java
@@ -0,0 +1,10 @@
+package javawork;
+
+public class work1 {
+
+	
+	// 메일제목 : 0329_flow1_김화동
+	// tbasic@daum.net
+	// 교육원 오는길 50단계
+	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HWADONG
+ *
+ */
+module javawork {
+}
